9c0159026167b6bac54ded7a309430ef83792003
justinqllvr <justin.quillevere@outlook.fr>
2024-06-03 00:23:09 +0200

    des belles flames

---
+++ src/webgl/materials/Flame/vertex.vs
@@ -0,0 +1,43 @@
+uniform float uTime;
+uniform float uSize;
+uniform vec2 uResolution;
+uniform float uScale;
+
+varying vec2 vUv;
+varying vec3 vPosition;
+
+attribute vec3 instancePosition;
+attribute float aSize, aSpeed;
+
+varying float vSize;
+varying float vSpeed;
+varying float vElevation;
+
+float PI = 3.141592653589793238;
+
+void main() {
+  vSize = aSize;
+  vSpeed = aSpeed;
+  vUv = uv;
+  float speed = 0.5;
+  float elevation = .05;
+
+  vec3 pos = position + instancePosition;
+  // pos += instancePosition;
+
+  //Life of flame trought size
+  float life = fract(-uTime * aSpeed * speed) * uScale;
+  float size = smoothstep(.0, .7, life) * smoothstep(1., .9, life) * aSize;
+  pos *= size;
+
+  //Position update
+  pos.y += fract(uTime * aSpeed * speed) * elevation * 2. + 0.05;
+  vElevation = pos.y;
+
+  vec4 modelPosition = modelMatrix * vec4(pos, 1.0);
+  vec4 viewPosition = viewMatrix * modelPosition;
+  vec4 projectionPosition = projectionMatrix * viewPosition;
+
+  gl_Position = projectionPosition;
+
+}
--- src/webgl/objects/Flame.js
+++ src/webgl/objects/Flame.js
@@ -5,56 +5,99 @@ import {
   DoubleSide,
   SphereGeometry,
 } from "three";
-import { TestPlaneMaterial } from "../materials/TestPlane/material";
 import gsap from "gsap";
 import { Vector3 } from "three";
+import { InstancedBufferGeometry } from "three";
+import { Vector2 } from "three";
+import { InstancedBufferAttribute } from "three";
+import { app } from "@/App";
+import { FlameMaterial } from "../materials/Flame/material";
+import { state } from "../../utils/State";
 
 export default class Flame extends Mesh {
   constructor() {
     super();
+    state.register(this);
+
+    this.count = 500;
+    this.resolution = new Vector2(app.viewport.width, app.viewport.height);
+    this.time = 0;
+    this.objectScale = { value: 0 };
+    this.maxRadius = 0.15;
 
     this.geometry = this.#createGeometry();
     this.material = this.#createMaterial();
   }
 
-  show() {
-    gsap.fromTo(
-      this.scale,
-      {
-        x: 0,
-        y: 0,
-        z: 0,
-      },
-      {
-        x: 1,
-        y: 1,
-        z: 1,
-        duration: 0.2,
-        ease: "power2.in",
-      }
-    );
-  }
-
-  hide() {
-    gsap.to(this.scale, {
-      x: 0,
-      y: 0,
-      z: 0,
-      duration: 0.2,
-    });
-  }
-
   #createGeometry() {
     const geometry = new SphereGeometry(0.04, 20, 20);
-    return geometry;
+    const instancedGeometry = new InstancedBufferGeometry().copy(geometry);
+    instancedGeometry.instanceCount = this.count;
+
+    // instance specific attributes
+    const positions = new Float32Array(this.count * 3);
+    const sizes = new Float32Array(this.count);
+    const speed = new Float32Array(this.count);
+
+    for (let i = 0; i < this.count; i++) {
+      const i3 = i * 3;
+      const angle = (i / this.count) * Math.PI * 2;
+      const radius = Math.sqrt(Math.random()) * this.maxRadius;
+
+      positions[i3] = Math.cos(angle) * radius;
+      positions[i3 + 1] = 0;
+      positions[i3 + 2] = Math.sin(angle) * radius;
+
+      sizes[i] = Math.random() * 0.2;
+      speed[i] = Math.random() * 0.5 + 0.5;
+    }
+
+    instancedGeometry.setAttribute(
+      "instancePosition",
+      new InstancedBufferAttribute(positions, 3)
+    );
+    instancedGeometry.setAttribute(
+      "aSize",
+      new InstancedBufferAttribute(sizes, 1)
+    );
+    instancedGeometry.setAttribute(
+      "aSpeed",
+      new InstancedBufferAttribute(speed, 1)
+    );
+
+    return instancedGeometry;
   }
 
   #createMaterial() {
-    const material = new MeshBasicMaterial({
-      color: "orange",
-      side: DoubleSide,
+    const material = new FlameMaterial({
+      uniforms: {
+        uSize: { value: this.particleSize },
+        uResolution: { value: this.resolution },
+        uTime: { value: this.time },
+        uScale: { value: this.objectScale.value },
+      },
     });
 
     return material;
+  }
+
+  onTick(e) {
+    this.material.uniforms.uTime.value = e.et / 1000;
+  }
+
+  show() {
+    gsap.to(this.material.uniforms.uScale, {
+      value: 1,
+      duration: 1,
+      ease: "power1.out",
+    });
+  }
+
+  hide() {
+    gsap.to(this.material.uniforms.uScale, {
+      value: 0,
+      duration: 1,
+      ease: "power1.in",
+    });
   }
 }
