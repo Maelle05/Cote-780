5807f1e5c4c2d64ba92c97570742bacc878bb45e
Maelle05 <maelle.rabouan@gmail.com>
2024-06-07 09:37:56 +0200

    meerge

--- src/utils/constants/config.js
+++ src/utils/constants/config.js
@@ -2,7 +2,7 @@ import { TEXTS } from "./texts";
 import dataText from "../../locales/fr.json";
 
 const DEV_MODE = true;
-const INIT_SCENE = 4;
+const INIT_SCENE = 5;
 
 const TITLES_SCENE = [
   "Intro",
