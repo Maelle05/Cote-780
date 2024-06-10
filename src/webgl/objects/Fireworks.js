193feceec1c9db84694817fb25acfdf7c5e235be
justinqllvr <justin.quillevere@outlook.fr>
2024-06-11 00:13:53 +0200

    fireworks still wip =)

--- src/webgl/objects/Fireworks.js
+++ src/webgl/objects/Fireworks.js
@@ -13,6 +13,7 @@ import { SpritesheetPlayer } from "../utils/SpritesheetPlayer";
 import { Group } from "three";
 import gsap from "gsap";
 import { state } from "@/utils/State";
+import { Color } from "three";
 
 export default class Fireworks {
   constructor(positions) {
@@ -20,7 +21,6 @@ export default class Fireworks {
     const explosionCount = 6;
     this.launchers = [];
     this.explosions = [];
-    this.tempExplosions = [];
 
     for (let i = 1; i <= explosionCount; i++) {
       //Create explosion mesh
@@ -32,8 +32,6 @@ export default class Fireworks {
       this.explosions.push(explosion);
     }
 
-    // this.tempExplosions = this.explosions.slice();
-
     for (let i = 0; i < positions.length; i++) {
       //Create launch mesh
       const mesh = this.#createMesh();
@@ -44,9 +42,16 @@ export default class Fireworks {
       this.launchers.push(mesh);
     }
 
+    this.clonedExplosions = [...this.explosions];
+    this.clonedLaunchers = [...this.launchers];
+
+    this.startFirework();
+
     return {
       launchers: this.launchers,
       explosions: this.explosions,
+      clonedExplosions: this.clonedExplosions,
+      clonedLaunchers: this.clonedLaunchers,
       play: this.play,
       getRandom: this.getRandom,
     };
@@ -57,18 +62,21 @@ export default class Fireworks {
   #createMesh() {
     const geometry = new PlaneGeometry(1, 1, 1, 1);
     const material = new FireworkMaterial({
-      uniforms: {},
+      uniforms: {
+        uProgress: { value: 0 },
+        uTime: { value: 0 },
+      },
       transparent: true,
     });
 
     const mesh = new Mesh(geometry, material);
-    mesh.scale.set(0.1, 0.1, 0.1);
+    mesh.scale.set(0.02, 0.02, 0.02);
 
     return mesh;
   }
 
   #createExplosion(spritesheet, id) {
-    const geometry = new PlaneGeometry(1, 1, 1, 1);
+    const geometry = new PlaneGeometry(2, 2, 1, 1);
     const material = spritesheet.material;
 
     const mesh = new Mesh(geometry, material);
@@ -79,15 +87,19 @@ export default class Fireworks {
   }
 
   play(launcher) {
-    const randomY = this.getRandom(4);
-    const randomIndex = Math.floor(Math.random() * this.explosions.length);
+    const randomY = this.getRandom(4, 1);
+    const randomIndex = Math.floor(
+      Math.random() * this.clonedExplosions.length
+    );
 
-    // console.log(this.tempExplosions);
+    const explosion = this.clonedExplosions.splice(randomIndex, 1)[0];
 
-    const explosion = this.explosions[randomIndex];
-
-    // console.log(explosion);
-
+    if (!explosion) return;
+    if (!launcher) return;
+    explosion.material.uniforms.uColor.value = new Color(
+      ...this.getRandomColor()
+    );
+    console.log(explosion.material.uniforms);
     //Anim the launcher
     gsap.to(launcher.position, {
       y: `${randomY}`,
@@ -97,19 +109,54 @@ export default class Fireworks {
         explosion.position.copy(launcher.position);
         explosion.lookAt(app.webgl.camera.position);
         explosion.spritesheet.playing = true;
+        launcher.position.y = 0;
+        this.clonedLaunchers.push(launcher);
       },
     });
   }
 
-  getRandom(value) {
-    const max = value + 1;
-    const min = value - 1;
+  startFirework() {
+    let randomTiming = this.getRandom(400, 200);
+
+    setInterval(() => {
+      const randomIndex = Math.floor(
+        Math.random() * this.clonedLaunchers.length
+      );
+
+      const launcher = this.clonedLaunchers.splice(randomIndex, 1)[0];
+
+      this.play(launcher);
+    }, randomTiming);
+  }
+
+  getRandom(value, interval) {
+    const max = value + interval;
+    const min = value - interval;
 
     return Math.random() * (max - min) + min;
   }
 
+  getRandomColor() {
+    const colors = [
+      [1.0, 0.0, 0.0], // Red
+      [0.0, 1.0, 0.0], // Green
+      [0.0, 0.0, 1.0], // Blue
+      [1.0, 1.0, 0.0], // Yellow
+      [1.0, 0.0, 1.0], // Magenta
+      [0.0, 1.0, 1.0], // Cyan
+      [1.0, 0.5, 0.0], // Orange
+      [0.5, 0.0, 0.5], // Purple
+      [0.0, 0.5, 0.5], // Teal
+      [0.5, 0.5, 0.5], // Gray
+    ];
+
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+  }
+
   onFireworksAnimStop(e) {
-    // console.log(e);
+    const explosion = this.explosions.filter((exp) => exp.textureID == e)[0];
+    this.clonedExplosions.push(explosion);
   }
 
   resize() {
